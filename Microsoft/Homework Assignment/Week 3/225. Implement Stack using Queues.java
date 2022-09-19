3d0d577cfa38570cfb1f615b5f0f8f4055540e9f
devamitra16 <109542385+devamitra16@users.noreply.github.com>
2022-09-19 20:01:28 +0530

    Create 225. Implement Stack using Queues.java

---
+++ Microsoft/Homework Assignment/Week 3/225. Implement Stack using Queues.java
@@ -0,0 +1,36 @@
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q=new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        int size=q.size()-1;
+        while(size >0){
+            q.add(q.poll());
+            size--;
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
