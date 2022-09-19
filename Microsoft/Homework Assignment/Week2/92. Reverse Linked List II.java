3398a242e2da028d997c8dd4fcbead665aef1bfc
devamitra16 <109542385+devamitra16@users.noreply.github.com>
2022-09-19 19:55:36 +0530

    Create 92. Reverse Linked List II.java

---
+++ Microsoft/Homework Assignment/Week2/92. Reverse Linked List II.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head==null || (left==right)){
+            return head;
+        }
+       ListNode temp;
+        if(left != 1){
+        temp=head;
+        }else{
+             temp=null;
+        }   
+        int count=1,flag=0;
+        ListNode newHead=null;
+        while(count<(left-1)){
+           temp=temp.next; 
+            count++;
+        }
+        count=left;
+        ListNode start;
+        if(temp==null){
+            start=head;
+        }else{
+        start=temp.next;
+        } 
+        while(count<=right ){
+           
+            ListNode next=start.next;
+            start.next=newHead;
+            newHead=start;
+            start=next;
+            count++;
+            
+        }
+        if(temp==null && start == null){
+            return newHead;
+        }else if(temp!=null && start!=null){
+        temp.next.next=start;
+        temp.next=newHead;
+        return head;
+        }else if(temp!=null && start==null){
+            temp.next=newHead;
+            return head;
+        }
+        else{
+            head.next=start;
+            return newHead;
+        }
+        
+    }
+}
