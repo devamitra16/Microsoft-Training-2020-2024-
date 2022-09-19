f5af491579cf7dd8db856203968a25143ff3a4d6
devamitra16 <109542385+devamitra16@users.noreply.github.com>
2022-09-19 11:19:00 +0530

    Create 15.add int with decimal.java

---
+++ Google Assignments/Assignment 1/15.add int with decimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int n1=sc.nextInt();
+   double n2=sc.nextDouble();
+  double sum=(double)n1 + n2;
+    System.out.println(sum);
+  }
+}
