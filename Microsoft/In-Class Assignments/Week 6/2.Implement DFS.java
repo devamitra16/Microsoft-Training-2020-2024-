ecf45d8507d9b238dcbfbc9f90d64312381d9f2a
devamitra16 <109542385+devamitra16@users.noreply.github.com>
2022-10-26 12:51:31 +0530

    Update 2.Implement DFS.java

--- Microsoft/In-Class Assignments/Week 6/2.Implement DFS.java
+++ Microsoft/In-Class Assignments/Week 6/2.Implement DFS.java
@@ -1,5 +1,7 @@
+//recursion
+
 class Solution {
-    // Function to return a list containing the DFS traversal of the graph.
+    
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         boolean[] visited=new boolean[V];
@@ -19,5 +21,34 @@ class Solution {
                 dfs(visited,adj,i,res);
             }
         }
+    }
+}
+
+
+*********************************************************************************************************************************************************************
+//Iterative
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        boolean[] visited=new boolean[V];
+        ArrayList<Integer> res=new ArrayList<>();
+        Stack<Integer> stack=new Stack<>();
+        visited[0]=true;
+        stack.push(0);
+        while(!stack.isEmpty()){
+        int s=stack.pop();
+        res.add(s);
+        for(int i=adj.get(s).size()-1;i>=0;i--){
+            int n=adj.get(s).get(i);
+            if(!visited[n]){
+                visited[n]=true;
+                stack.push(n);
+            }
+        
+        }
+        }
+        return res;
     }
 }
