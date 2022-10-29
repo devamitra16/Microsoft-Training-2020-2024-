e111f216bde5070b7d3278917b3f2fc517fe5608
devamitra16 <109542385+devamitra16@users.noreply.github.com>
2022-10-29 14:58:13 +0530

    Create 3.Remove linked list elements.java

---
+++ Microsoft/In-Class Assignments/Week 8/3.Remove linked list elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null){
+            return null;
+        }
+       if(head.val==val){
+           return removeElements(head.next,val);
+       }else{
+           head.next=removeElements(head.next,val);
+       }
+       return head;
+    }
+}
